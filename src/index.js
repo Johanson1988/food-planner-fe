82f2f88e602455ec1124358dc252d38b7e51bcc2
Johann Moreno <johanson88@gmail.com>
2020-06-10 16:55:22 +0200

    add redux looger

--- src/index.js
+++ src/index.js
@@ -7,14 +7,15 @@ import { createStore, applyMiddleware } from 'redux';
 import rootReducer from './store/reducers/rootReducer';
 import { Provider } from 'react-redux';
 import thunk from 'redux-thunk';
+import logger from 'redux-logger';
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+const store = createStore(rootReducer, applyMiddleware(thunk, logger));
 
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Provider store={store}><App /> </Provider>
+    <Provider store={store}><App /></Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
