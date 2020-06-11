36ad9135ce00ff08e60582660d34d31100f31c71
Johann Moreno <johanson88@gmail.com>
2020-06-11 18:05:35 +0200

    table displaying ingredient details

--- src/components/app/ingredient/IngredientDetails.js
+++ src/components/app/ingredient/IngredientDetails.js
@@ -1,21 +1,81 @@
-import React from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { getIngredients } from './../../../store/actions/ingredientActions';
 
-const IngredientDetails = (props) => {
-    const id = props.match.params.id;
-    return (
-        <div className="container section ingredient-details">
-            <div className="card z-depth-0">
-                <div className="card-content">
-                    <span className="card-title">Ingredient - {id}</span>
-                    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi perspiciatis est laborum delectus, iste quos neque minus laudantium eaque a ex esse maxime non debitis ipsum ullam vel qui dolores!</p>
-                </div>
-                <div className="card-action grey lighten-4 grey-text">
-                    <div>Posted by Rayban</div>
-                    <div>20th Of May</div>
+class IngredientDetails extends Component {
+
+    componentDidMount() {
+        this.props.getIngredients(`_id=${this.props.match.params.id}`)
+    }
+    
+    render () {
+
+        return (
+            <div className="container section ingredient-details">
+                <div className="card z-depth-0">
+                    <div className="card-content">
+                        <span className="card-title">{this.props.ingredient[0] ? this.props.ingredient[0].name : ''}</span>
+                        <table>
+                            <tbody>
+                                <tr>
+                                    <td>kcal</td>
+                                    <td>{this.props.ingredient[0] ? this.props.ingredient[0].kcal : ''}</td>
+                                </tr>
+                                <tr>
+                                    <td>fats</td>
+                                    <td>{this.props.ingredient[0] ? this.props.ingredient[0].fats : ''}</td>
+                                </tr>
+                                <tr>
+                                    <td>saturatedFats</td>
+                                    <td>{this.props.ingredient[0] ? this.props.ingredient[0].saturatedFats : ''}</td>
+                                </tr>
+                                <tr>
+                                    <td>carboHydrates</td>
+                                    <td>{this.props.ingredient[0] ? this.props.ingredient[0].carboHydrates : ''}</td>
+                                </tr>
+                                <tr>
+                                    <td>sugar</td>
+                                    <td>{this.props.ingredient[0] ? this.props.ingredient[0].sugar : ''}</td>
+                                </tr>
+                                <tr>
+                                    <td>proteins</td>
+                                    <td>{this.props.ingredient[0] ? this.props.ingredient[0].proteins : ''}</td>
+                                </tr>
+                                <tr>
+                                    <td>salt</td>
+                                    <td>{this.props.ingredient[0] ? this.props.ingredient[0].salt : ''}</td>
+                                </tr>
+                                <tr>
+                                    <td>brand</td>
+                                    <td>{this.props.ingredient[0] ? this.props.ingredient[0].brand : ''}</td>
+                                </tr>
+                                <tr>
+                                    <td>fiber</td>
+                                    <td>{this.props.ingredient[0] ? this.props.ingredient[0].fiber : ''}</td>
+                                </tr>
+                            </tbody>
+                        </table>
+                    </div>
+                    <div className="card-action grey lighten-4 grey-text">
+                        <div>Posted by Rayban</div>
+                        <div>20th Of May</div>
+                    </div>
                 </div>
             </div>
-        </div>
-    )
+        )
+    }
 }
 
-export default IngredientDetails;
+const mapStateToProps = state => {
+    return {
+        ingredient: state.ingredient
+    }
+}
+
+const mapDispatchToProps = dispatch => {
+    return {
+        getIngredients: (_id) => dispatch(getIngredients(_id)),
+    }
+}
+
+export default connect(mapStateToProps, mapDispatchToProps) (IngredientDetails);
