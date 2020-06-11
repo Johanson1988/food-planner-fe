6b5b55024618186dcc05326bc28843ca620056dc
Johann Moreno <johanson88@gmail.com>
2020-06-11 21:12:58 +0200

    delete unnecesary console.logs

--- src/store/reducers/foodReducer.js
+++ src/store/reducers/foodReducer.js
@@ -14,24 +14,20 @@ const initState = {
 const foodReducer = (state = initState, action) => {
     switch (action.type) {
         case 'ADD_FOOD':
-            console.log('Food added: ', action.food);
             return state;
         case 'ADD_FOOD_ERROR':
             console.log('create food error', action.error);
             return state;
         case 'ADD_INGREDIENT_TO_FOOD':
-            console.log('Ingredient added to foods: ', action.ingredient);
             const ingredients = [...state.ingredients, action.ingredient];
             return { ...state, ingredients };
         case 'ADD_INGREDIENT_TO_FOOD_ERROR':
             console.log('Ingredient added to food error: ', action.error);
             return state;
         case 'DELETE_INGREDIENT_FROM_FOOD':
-            console.log('Deleted ingredient from food: ', action.ingredientId);
             const newIngredients = state.ingredients.filter(ingredient => ingredient._id !== action.ingredientId);
             return { ...state, ingredients: newIngredients };
         case 'UPDATE_FOOD_PROPERTY':
-            console.log('Updated food property', action.foodProperty);
             return { ...state, ...action.foodProperty };
         default:
             return state;
