284bf8548224c46fe65b9f5d3d55d8f6ac6ffee9
Johann Moreno <johanson88@gmail.com>
2020-06-16 19:38:10 +0200

    Food summary displaying food details

--- src/components/app/food/FoodSummary.js
+++ src/components/app/food/FoodSummary.js
@@ -2,10 +2,20 @@ import React from 'react';
 
 const FoodSummary = (props) => {
     console.log(props.food.name);
-    const {name} = props.food;
+    const { name, kcal, fats, saturatedFats, carboHydrates, sugar, proteins, salt, fiber, date, mealType } = props.food;
     return (
         <tr>
             <td>{name}</td>
+            <td>{kcal}</td>
+            <td>{fats}</td>
+            <td>{saturatedFats}</td>
+            <td>{carboHydrates}</td>
+            <td>{sugar}</td>
+            <td>{proteins}</td>
+            <td>{salt}</td>
+            <td>{fiber}</td>
+            <td>{date}</td>
+            <td>{mealType}</td>
         </tr>
     )
 }
